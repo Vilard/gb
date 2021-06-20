d14eeec4000627b553c43bf5b3a132f1867be6b1
Vilard <vladimir052290@mail.ru>
2021-06-20 07:34:14 +0300

    less8

---
+++ pybasic/less8/task1.py
@@ -0,0 +1,34 @@
+# Написать функцию email_parse(<email_address>), 
+# которая при помощи регулярного выражения извлекает 
+# имя пользователя и почтовый домен из email адреса 
+# и возвращает их в виде словаря. Если адрес не валиден, 
+# выбросить исключение ValueError. Пример:
+# >>> email_parse('someone@geekbrains.ru')
+# {'username': 'someone', 'domain': 'geekbrains.ru'}
+# >>> email_parse('someone@geekbrainsru')
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+#   ...
+#     raise ValueError(msg)
+# ValueError: wrong email: someone@geekbrainsru
+
+# Примечание: подумайте о возможных ошибках в адресе и постарайтесь 
+# учесть их в регулярном выражении; имеет ли смысл в 
+# данном случае использовать функцию re.compile()?
+
+import re
+
+def email_parse(mail):
+    try:
+        dct = {}
+        RE = re.compile('^([a-z0-9]+)@([a-z]+.[a-z]+$)')
+        lst = list(RE.findall(mail)[0])
+        dct.update({"username": lst[0], "domain": lst[1]})
+        return dct
+    except IndexError:
+        print("not valid email")
+
+str_mail = 'someone@geekbrains.ru'
+
+print(email_parse(str_mail))
+
