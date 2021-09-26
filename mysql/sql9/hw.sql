eb00bfc73f61c0702a615294cf67f14e38aa662f
vladimir <vladimir052290@mail.ru>
2021-09-26 05:26:57 +0300

    9.3.1

--- mysql/sql9/hw.sql
+++ mysql/sql9/hw.sql
@@ -3,4 +3,12 @@ begin;
 insert sample.users select * from shop.users where id = 1;
 commit;
 --2
+create view pcname as select products.name as pname, catalogs.name
+from products join catalogs using(id);
+select * from pcname;
+
+-- 3.1
+DELIMITER $$
+create function hello()
+
 
