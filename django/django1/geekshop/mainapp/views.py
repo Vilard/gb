de3526514bf41eb3ab9a0d23d411853aa567823d
Vladimir <vladimir052290@mail.ru>
2022-03-03 15:28:32 +0300

    update 03.03.2022

--- django/django1/geekshop/mainapp/views.py
+++ django/django1/geekshop/mainapp/views.py
@@ -1,25 +1,36 @@
-from django.shortcuts import render
-from mainapp.models import Product
+from unicodedata import category
+from django.shortcuts import get_object_or_404, render
+from mainapp.models import Product, ProductCategory
 
 # Create your views here.
-def products(request, pk):
+def products(request, pk=None):
     title = 'Продукты'
+    link_menu = ProductCategory.objects.all()
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = ProductCategory.filter(category__pk=pk).order_by('price')
     
-    link_menu = [
-        {"href": "products_all", "name": "все"},
-        {"href": "products_home", "name": "дом"},
-        {"href": "products_office", "name": "офис"},
-        {"href": "products_modern", "name": "модерн"},
-        {"href": "products_classic", "name": "классика"},
-    ]
+        context = {
+        'title': title,
+        'link_menu': link_menu,
+        'products': products,
+        'category': category,
+        
+        }    
     
-    product = Product.objects.get(id=pk)
+        return render(request, "mainapp/products.html", context)
+
+    some_product = Product.objects.all()
     
     context = {
         'title': title,
-        'link_menu': link_menu
-    }
-    
+        'link_menu': link_menu,
+        'products': some_product,
+        }    
     
     return render(request, "mainapp/products.html", context)
-
+    
